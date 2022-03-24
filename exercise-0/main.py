527833ca74baf2d92884ef3e31fabda1be2c61fd
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-03-24 20:40:20 +0545

    first commit

---
+++ exercise-0/main.py
@@ -0,0 +1,71 @@
+# print(course)
+# print(len(course))
+# print(5+3)
+# print(5/3)
+# print(5%3)
+# print(5**3)
+# print(5//3)
+# print(5+3*2-10/5)
+# print((5+3)*5-2**3+40/4)
+
+
+# is_hot = True
+# is_cold = False
+# if is_hot:
+#     print("it's a hot day")
+#     print('drink plenty of water')
+# elif is_cold:
+#     print("it's a cold day")
+#     print('wear warm clothes')
+# else:
+#     print("it's neither hot nor cold")
+# print('enjoy your day!')
+
+
+        # Calculate down payment
+# credit = True
+# price = float(input('enter the price of the house: '))
+# if credit == True:
+#     down_payment = price * 0.1
+#     print('You need to put down 10%')
+#     print(f"Your down payment amount should be {down_payment}")
+# else:
+#     down_payment = price * 0.2
+#     print('You need to put down 20%')
+#     print(f"Your down payment amount should be {down_payment}")
+
+
+        # Assignment Operators
+# good_credit = True
+# high_income = True
+# criminal_record = False
+# if good_credit and high_income and not criminal_record:
+#     print('eligible for loan')
+# else:
+#     print('not eligible for loan')
+
+
+
+
+       # Calculation with greater than less than
+temp = int(input('enter the current temperature: '))
+hot = temp > 30
+cold = temp < 10
+
+if hot:
+    print("It's a hot day")
+elif cold:
+    print("It's a cold day")
+else:
+    print("It's neither hot nor cold")
+
+
+                # Assignment
+# If password is less than 8 characters long
+#     password length must be greater then 8 CHARACTERS
+# Otherwise if it's more than 10 characters long
+#     password length must not be greater than 10 characters
+# otherwise
+#     password is set    
+    
+    
